4994e771407af02b697348c5d938286220ed625e
Cristofferb7 <136852733+Cristofferb7@users.noreply.github.com>
2023-07-11 02:05:44 -0400

    made an if statements to log the time into the html and also register when to add and remove classes that changes the color of each container

--- assets/script.js
+++ assets/script.js
@@ -33,6 +33,26 @@ $("#currentDay").html(todayDate);
         $(".time-block").each(function () {
           var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
+          // To check the time and add the classes for background indicators
+          if (blockTime < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+
+        }
+    })
+}
+
   
 
 
